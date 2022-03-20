19b79fc6c2b5ee3a577d9c4071b81fdc894a2239
Edison Lien <fvckedison@gmail.com>
2022-03-20 10:42:48 +0800

    run as admin /fix logic

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ import keyboard
 import pyautogui
 import win32gui
 import cv2
+import time
 from PyQt5.QtWidgets import QApplication
 import sys
 hwnd = win32gui.FindWindow(0, 'MixMaster Online')
@@ -22,22 +23,45 @@ def convertQImageToMat(incomingImage):
 
 def heal(hp):
     if hp==80:
-        pyautogui.typewrite(['num1', 'f1', 'num8'])
-        print("50%")
+        pyautogui.press('1')
+        time.sleep(0.1)
+        pyautogui.press('f1')
+        time.sleep(0.1)
+        pyautogui.press('8')
+        time.sleep(0.1)
         print("80%")
         
     elif hp==60:
-        pyautogui.typewrite(['num1', 'f1', 'num8'])
+        pyautogui.press('1')
+        time.sleep(0.1)
+        pyautogui.press('f1')
+        time.sleep(0.1)
+        pyautogui.press('8')
+        time.sleep(0.1)
         print("50%")
     elif hp==40:
-        pyautogui.typewrite(['num1', 'f1', 'num8'])
+        pyautogui.press('1')
+        sleep(0.1)
+        pyautogui.press('f1')
+        sleep(0.1)
+        pyautogui.press('8')
+        sleep(0.1)        
         print("50%")
     elif hp==40:
-        pyautogui.typewrite(['num1', 'f1', 'num8'])
+        pyautogui.press('1')
+        time.sleep(0.1)
+        pyautogui.press('f1')
+        time.sleep(0.1)
+        pyautogui.press('8')
+        time.sleep(0.1)        
         print("40%")
-        update_res()
     elif hp==20:
-        pyautogui.typewrite(['num1', 'f1', 'num8'])
+        pyautogui.press('1')
+        time.sleep(0.1)
+        pyautogui.press('f1')
+        time.sleep(0.1)
+        pyautogui.press('8')
+        time.sleep(0.1)
         print("30%")
 
 
@@ -57,13 +81,11 @@ def generate_img():
     data=convertQImageToMat(data)#將獲取的影象從QImage轉換為RBG格式
     cv2.imwrite('output.png', data)
 while res==1:
-    if keyboard.is_pressed('b'):
-        pyautogui.press('F1')
-    # generate_img()
-    # img = cv2.imread('output.png')
-    # role = [img[22, 758],img[33, 747],img[40, 746],img[55, 749],img[65, 753]]
-    # pet1 = [img[22, 823],img[33, 813],img[40, 813],img[55, 814],img[65, 819]]
-    # pet2 = [img[22, 888],img[33, 880],img[40, 878],img[55, 880],img[65, 888]]
-    # pet3 = [img[22, 952],img[33, 942],img[40, 943],img[55, 943],img[65, 951]]
-    # heal(calc_status(role))
+    generate_img()
+    img = cv2.imread('output.png')
+    role = [img[22, 758],img[33, 747],img[40, 746],img[55, 749],img[65, 753]]
+    pet1 = [img[22, 823],img[33, 813],img[40, 813],img[55, 814],img[65, 819]]
+    pet2 = [img[22, 888],img[33, 880],img[40, 878],img[55, 880],img[65, 888]]
+    pet3 = [img[22, 952],img[33, 942],img[40, 943],img[55, 943],img[65, 951]]
+    heal(calc_status(role))
 # print(calc_status(role)) #[32, 31, 47]
