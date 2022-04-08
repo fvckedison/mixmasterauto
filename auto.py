e8fd65f5a32813fbb4c8bd011e8e12f44c031e7e
Edison Lien <fvckedison@gmail.com>
2022-04-08 23:55:31 +0800

    fix intertupt with mouse enter

--- auto.py
+++ auto.py
@@ -1,17 +1,20 @@
 import pyautogui
 import time
 import keyboard
-import time
+from ctypes import *
 import threading
-import time
-press=[0,0]
+press=[0,0,0,0]
 pet=[0,0,0,0,0,0]
 
 def get_position():
     global press 
     global pet
     if keyboard.is_pressed('f10'):  
-        press=[pyautogui.position()[0],pyautogui.position()[1]]
+        press[0]=pyautogui.position()[0]
+        press[1]=pyautogui.position()[1]
+    elif (keyboard.is_pressed('f12')):
+        press[2]=pyautogui.position()[0]
+        press[3]=pyautogui.position()[1]
     elif (keyboard.is_pressed('l')):
         pet[0]=pyautogui.position()[0]
         pet[1]=pyautogui.position()[1]
@@ -26,21 +29,32 @@ def press_f2():
     
     while True:
         print(press)
-        if(press[0]!=0 or press[1]!=0):
+        if(press[0]!=0 and press[1]!=0 and press[2]!=0 and press[3]!=0):
+            windll.user32.BlockInput(True)
             pyautogui.keyDown('f2')
             pyautogui.keyUp('f2')
-            pyautogui.moveTo(press[0], press[1],0.1)
-            pyautogui.click(clicks=1, interval=0.5, button='left')
+            pyautogui.moveTo(press[0], press[1],0.0)
+            pyautogui.click(clicks=1, interval=0.0, button='left')
+            
+            pyautogui.moveTo(press[2], press[3],0.0)
+            pyautogui.keyDown('8')
+            pyautogui.keyUp('8')
+            windll.user32.BlockInput(False)
             time.sleep(5)
 
 def press_f4():
     while True:
-        print(press)
-        if(press[0]!=0 or press[1]!=0):
+        print('gg')
+        if(press[0]!=0 and press[1]!=0 and press[2]!=0 and press[3]!=0):
             pyautogui.keyDown('f4')
             pyautogui.keyUp('f4')
-            pyautogui.moveTo(press[0], press[1],0.1)
-            pyautogui.click(clicks=1, interval=0.5, button='left') 
+            pyautogui.moveTo(press[0], press[1],0.0)
+            pyautogui.click(clicks=1, interval=0.0, button='left')
+            windll.user32.BlockInput(True)
+            pyautogui.moveTo(press[2], press[3],0.0)
+            pyautogui.keyDown('8')
+            pyautogui.keyUp('8')
+            windll.user32.BlockInput(False)
             time.sleep(100)
 
 
@@ -56,6 +70,7 @@ def press_f7():
 
 
 if __name__ == "__main__":
+
     t2 = threading.Thread(target = press_f2)
     t2.setDaemon(True)
     t2.start()
@@ -63,10 +78,6 @@ if __name__ == "__main__":
     t4 = threading.Thread(target = press_f4)
     t4.setDaemon(True)
     t4.start()
-
-    t7 = threading.Thread(target = press_f7)
-    t7.setDaemon(True)
-    t7.start()
     while True:
-        get_position()
         pass
+        get_position()
